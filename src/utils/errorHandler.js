0a497f60868763f526d32e3b5aee7fa3eb3cda99
Tobi Akanji <tobia807@gmail.com>
2020-06-07 10:10:38 +0100

    rf(register-community):remove console log[Fixes #10]

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,3 @@
-module.exports.dbQueryErrorHandler = (err) => {
-  console.log(err);
-  return {
-    error: (res) => res.status(500).send('Internal server error')
-  };
-};
+module.exports.dbQueryErrorHandler = () => ({
+  error: (res) => res.status(500).send('Internal server error')
+});
